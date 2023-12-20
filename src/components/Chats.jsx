379a2edf01db194cc5bfd53612b250e09f1acdf5
Shivam <singhkumarshivam15@gmail.com>
2023-12-20 07:38:22 +0530

    deploy

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -2,9 +2,39 @@ import React from 'react'
 
 const Chats = () => {
   return (
-    <div>
-      
+    <div className='chats'>
+      <div className="userChat">
+        <img src='https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt=''/>
+        <div className="userChatInfo">
+          <span>Shivam</span>
+        </div>
+      </div>
+      <div className="userChat">
+        <img src='https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt=''/>
+        <div className="userChatInfo">
+          <span>Shivam</span>
+        </div>
+      </div>
+      <div className="userChat">
+        <img src='https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt=''/>
+        <div className="userChatInfo">
+          <span>Shivam</span>
+        </div>
+      </div>
+      <div className="userChat">
+        <img src='https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt=''/>
+        <div className="userChatInfo">
+          <span>Shivam</span>
+        </div>
+      </div>
+      <div className="userChat">
+        <img src='https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt=''/>
+        <div className="userChatInfo">
+          <span>Shivam</span>
+        </div>
+      </div>
     </div>
+    
   )
 }
 
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 import Navbar from "./NavBar"
 import Search  from "./Search"
-// import Chats  from "./Chats"
+import Chats  from "./Chats"
 
 const Sidebar = () => {
   return (
     <div className='sidebar'>
       <Navbar/>
       <Search/>
-      {/* <Chats/> */}
+      <Chats/>
     </div>
   )
 }

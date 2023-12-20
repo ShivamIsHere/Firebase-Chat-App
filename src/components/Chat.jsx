23fcc4a1c3687243cf02c207ca30975f9725addc
Shivam <singhkumarshivam15@gmail.com>
2023-12-20 09:18:02 +0530

    created chat layout

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-<<<<<<< HEAD
 import Cam from "/images/cam.png"
 import Add from "/images/add.png"
 import More from "/images/more.png"
@@ -7,13 +6,9 @@ import Messages from './Messages'
 import Input from './Input'
 
 
-=======
->>>>>>> 379a2edf01db194cc5bfd53612b250e09f1acdf5
-
 const Chat = () => {
   return (
     <div className='chat'>
-<<<<<<< HEAD
       <div className='chatInfo'>
         <span>Shivam</span>
         <div className='chatIcons'>
@@ -24,9 +19,6 @@ const Chat = () => {
       </div>
       {/* <Messages/>
       <Input/> */}
-=======
-      chat
->>>>>>> 379a2edf01db194cc5bfd53612b250e09f1acdf5
     </div>
   )
 }

820e20b3d301306c53cb913a85e873d580f9e263
Shivam <singhkumarshivam15@gmail.com>
2023-12-27 03:14:53 +0530

    working on search component

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -1,17 +1,55 @@
-import React from 'react'
+import React, { useState,useContext } from 'react'
+import {
+  collection,
+  query,
+  where,
+  getDocs,
+  setDoc,
+  doc,
+  updateDoc,
+  serverTimestamp,
+  getDoc,
+} from "firebase/firestore";
+
+import { AuthContext } from "../context/AuthContext";
+import {db} from "../firebase"
 
 const Search = () => {
+
+  const [username, setUsername] =useState("");
+  const [user, setUser] =useState(null);
+  const [err, setErr] =useState(false);
+
+  const handleSearch=async ()=>{
+    const q = query(collection(db,"users"),where("displayName", "==" , username))
+    try {
+      const querySnapshot = await getDocs(q);
+      querySnapshot.forEach((doc) => {
+        setUser(doc.data());
+      });
+    } catch (err) {
+      setErr(true);
+    }
+  };
+
+  const handleKey=(e)=>{
+    e.code==="Enter" && handleSearch();
+    
+    
+  }
+
   return (
     <div className='search'>
-      <div className='searchForm'>
-        <input type='text' placeholder='Find a User'/>
+       <div className='searchForm'>
+        <input type='text' placeholder='Find a User' onKeyDown={handleKey} onChange={e=>setUsername(e.target.value)}/>
       </div>
-      <div className="userChat">
-        <img src='https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt=''/>
+      {err && <span>User not found!!!</span>}
+      {user &&<div className="userChat">
+        <img src={user.photoURL} alt=''/>
         <div className="userChatInfo">
-          <span>Shivam</span>
+          <span>{user.displayName}</span>
         </div>
-      </div>
+      </div>}
     </div>
   )
 }

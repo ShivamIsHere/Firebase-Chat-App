3a3212707b7bfe164dc988e47f633a60b1ea5f10
Shivam <singhkumarshivam15@gmail.com>
2023-12-27 02:50:32 +0530

    changing name and profile pic of user according to the login user

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,15 +1,17 @@
 import { signOut } from 'firebase/auth';
-import React from 'react'
+import React, { useContext } from 'react'
 import { RxAvatar } from "react-icons/rx";
 import { auth } from '../firebase';
+import { AuthContext } from '../context/AuthContext';
 
 const NavBar = () => {
+  const {currentUser}= useContext(AuthContext)
   return (
     <div className='navbar'>
       <spam className="logo">Giga Chat</spam>
       <div className="user">
-      <img src='https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt=''/>
-      <spam>Shivam</spam>
+      <img src={currentUser.photoURL} alt=''/>
+      <spam>{currentUser.displayName}</spam>
       <button onClick={()=>signOut(auth)}>Logout</button>
       </div>
       

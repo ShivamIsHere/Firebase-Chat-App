94c596bc981e43147a276e20e350dd4cf63f461b
Shivam <singhkumarshivam15@gmail.com>
2024-01-19 00:19:44 +0530

    connecting two chats so that we can chat with anyone

--- src/components/Chats.jsx
+++ src/components/Chats.jsx
@@ -6,14 +6,14 @@ const Chats = () => {
       <div className="userChat">
         <img src='https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt=''/>
         <div className="userChatInfo">
-          <span>Shivam</span>
+          <span>Shiv</span>
         </div>
       
       </div>
       <div className="userChat">
         <img src='https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt=''/>
         <div className="userChatInfo">
-          <span>Shivam</span>
+          <span>john</span>
         </div>
       
       </div>

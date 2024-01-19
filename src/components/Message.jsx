1cc865e919e9c7374d8c8e23529d4109fa8b3d87
Shivam <singhkumarshivam15@gmail.com>
2024-01-19 20:03:59 +0530

    Fetching  Private Chat Message

--- src/components/Message.jsx
+++ src/components/Message.jsx
@@ -1,16 +1,25 @@
 import React from 'react'
+import { AuthContext } from "../context/AuthContext";
+import { ChatContext } from "../context/ChatContext";
 
-const Message = () => {
+const Message = ({message}) => {
+
+  const { currentUser } = useContext(AuthContext);
+  const { data } = useContext(ChatContext);
+
+  
+
+
   return (
     <div className='message owner'>
-      <div className='messageInfo'>
+      {/* <div className='messageInfo'>
       <img src='https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt=''/>
       <span>just now</span>
       </div>
       <div className='messageContent'>
         <p>hello</p>
         <img src='https://images.pexels.com/photos/104827/cat-pet-animal-domestic-104827.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1' alt=''/>
-      </div>
+      </div> */}
     </div>
   )
 }
--- src/components/Messages.jsx
+++ src/components/Messages.jsx
@@ -1,21 +1,29 @@
 import React from 'react'
 import Message from "./Message"
+import { ChatContext } from '../context/ChatContext';
 
 const Messages = () => {
+
+  const [messages, setMessages] = useState([]);
+  const { data } = useContext(ChatContext);
+
+  useEffect(() => {
+    const unSub = onSnapshot(doc(db, "chats", data.chatId), (doc) => {
+      doc.exists() && setMessages(doc.data().messages);
+    });
+
+    return () => {
+      unSub();
+    };
+  }, [data.chatId]);
+
+  console.log(messages)
+
   return (
     <div className='messages'>
-      <Message/>
-      <Message/>
-      <Message/>
-      <Message/>
-      <Message/>
-      <Message/>
-      <Message/>
-      <Message/>
-      <Message/>
-      <Message/>
-      <Message/>
-
+      {messages.map((m) => (
+        <Message message={m} key={m.id} />
+      ))}
     </div>
   )
 }
